7ce5aef31372a9e3602276912603431c8f3fac5d
RaviKumarNerusu <2300033344cse@gmail.com>
2025-08-11 10:41:51 +0530

    dundi vysh

---
+++ src/main/java/com/example/dundi/CicdBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.dundi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdBackend1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dundi/CicdBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dundi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdBackend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

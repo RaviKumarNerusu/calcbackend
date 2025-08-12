43a920ed36d6fa644c3b889ff1f0d9a757d26f72
RaviKumarNerusu <2300033344cse@gmail.com>
2025-08-12 21:55:54 +0530

    utham

--- src/main/java/com/example/dundi/CicdBackend1Application.java
+++ src/main/java/com/example/dundi/CicdBackend1Application.java
@@ -2,12 +2,18 @@ package com.example.dundi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdBackend1Application {
+public class CicdBackend1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdBackend1Application.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CicdBackend1Application.class);
+	}
 }

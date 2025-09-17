12a3ed3b0198142d2e0b32df004df269124d205a
RAVI KUMAR NERUSU <163744019+RaviKumarNerusu@users.noreply.github.com>
2025-09-17 15:41:27 +0530

    Update CicdBackend1Application.java

--- src/main/java/com/example/dundi/CicdBackend1Application.java
+++ src/main/java/com/example/dundi/CicdBackend1Application.java
@@ -3,7 +3,10 @@ package com.example.dundi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.ErrorPageFilter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CicdBackend1Application extends SpringBootServletInitializer {
@@ -16,5 +19,14 @@ public class CicdBackend1Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(CicdBackend1Application.class);
+    }
+
+    // Fix for the Tomcat deployment error.
+    // This disables the duplicate registration of the ErrorPageFilter.
+    @Bean
+    public FilterRegistrationBean<ErrorPageFilter> errorPageFilterRegistration(ErrorPageFilter filter) {
+        FilterRegistrationBean<ErrorPageFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setEnabled(false);
+        return registration;
     }
 }
